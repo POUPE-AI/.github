cfef3903a4623516f146e36428fbd3a6bdfc9091
Erisvaldo <erisvaldoapps@gmail.com>
2025-07-14 22:25:00 -0300

    feat: full kong config

---
+++ docker/kong/plugins/jwt-header-injector/handler.lua
@@ -0,0 +1,20 @@
+local JwtHeaderInjector = {
+  PRIORITY = 1000,
+  VERSION = "1.0.0",
+}
+
+function JwtHeaderInjector:access(conf)
+  local jwt_parser = require "kong.plugins.jwt.jwt_parser"
+  local authorization = kong.request.get_header("authorization")
+  if authorization then
+    local _, _, token = authorization:find("Bearer%s+(.+)")
+    if token then
+      local jwt, err = jwt_parser:new(token)
+      if not err and jwt.claims and jwt.claims.sub then
+        kong.service.request.set_header("X-User-Sub", jwt.claims.sub)
+      end
+    end
+  end
+end
+
+return JwtHeaderInjector
